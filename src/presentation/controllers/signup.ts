85f2efbb128419b256585afbc1845764407f8436
Cosmo Abdon <cosmoabdon@2bebot.com>
2022-03-16 09:16:22 -0300

    feat: ensure SignUpController returns 500 if EmailValidor throws

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -6,6 +6,7 @@ import { IHttpRequest, IHttpResponse } from '../protocols/http'
 import { Controller } from '../protocols/controller'
 import { EmailValidator } from '../protocols/email-validator'
 import { InvalidParamError } from '../errors/invalid-param-error'
+import { ServerError } from '../errors/server-error'
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
@@ -15,16 +16,23 @@ export class SignUpController implements Controller {
   }
 
   handle (httpRequest: IHttpRequest): IHttpResponse {
-    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field))
+    try {
+      const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
+      }
+
+      const isValid = this.emailValidator.isValid(httpRequest.body.email)
+
+      if (!isValid) { return badRequest(new InvalidParamError('email')) }
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: new ServerError()
       }
     }
-
-    const isValid = this.emailValidator.isValid(httpRequest.body.email)
-
-    if (!isValid) { return badRequest(new InvalidParamError('email')) }
 
     return {
       statusCode: 200,
