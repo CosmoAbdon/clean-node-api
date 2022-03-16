d34f4a751165d155b12b441443dc1df74517d1aa
Cosmo Abdon <cosmoabdon@2bebot.com>
2022-03-16 05:09:07 -0300

    refactor: simplify code

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -6,16 +6,11 @@ import { IHttpRequest, IHttpResponse } from '../protocols/http'
 
 export class SignUpController {
   handle (httpRequest: IHttpRequest): IHttpResponse {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingParamError('name'))
-    }
-    if (!httpRequest.body.email) {
-      return badRequest(new MissingParamError('email'))
-    }
-
-    return {
-      statusCode: 200,
-      body: 'Success'
+    const requiredFields = ['name', 'email']
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field))
+      }
     }
   }
 }
