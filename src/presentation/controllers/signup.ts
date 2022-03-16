3fbce01ceca144ac58cccff72a97350633c2df2e
Cosmo Abdon <cosmoabdon@2bebot.com>
2022-03-16 10:02:50 -0300

    refactor: remove code duplicated

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -18,9 +18,11 @@ export class SignUpController implements Controller {
         }
       }
 
-      const isValid = this.emailValidator.isValid(httpRequest.body.email)
+      const { email, password, passwordConfirmation } = httpRequest.body
 
-      if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) { return badRequest(new InvalidParamError('passwordConfirmation')) }
+      if (password !== passwordConfirmation) { return badRequest(new InvalidParamError('passwordConfirmation')) }
+
+      const isValid = this.emailValidator.isValid(email)
 
       if (!isValid) { return badRequest(new InvalidParamError('email')) }
     } catch (error) {
